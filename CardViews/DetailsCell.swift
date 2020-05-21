1954061df5479a169ddb68d98b7c8a590bf9ef98
Narendra Anam <narendra.a431@gmail.com>
2020-05-21 16:31:24 +0530

    Initial commit

---
+++ CardViews/DetailsCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsCell.swift
+//  CardViews
+//
+//  Created by Narendra Anam on 21/05/20.
+//  Copyright © 2020 NarendraAnam. All rights reserved.
+//
+
+import UIKit
+
+class DetailsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
